22711009c9cb441a4461da08f52872c10713d103
Zoran Lazic <ezelohar@gmail.com>
2014-03-05 01:06:48 +0100

    add new updates to project

--- app/game.js
+++ app/game.js
@@ -8,6 +8,8 @@
 
 var peggle = function () {
 	
+	/* Game engine */
+	this.game = new Phaser.Game(1024, 768, Phaser.AUTO, '', { preload: this.preload, create: this.create, update: this.update });
 	
 	
 	/* Game Stats */
@@ -38,21 +40,142 @@ var peggle = function () {
 	this.elements = {
 		'balls': []
 	};
+	this.shutter = null;
+	this.elementsCount = 100;
 	
 	/* Load Data */
 	
 	/* Helpers */
 	
 	/* Animate request id */
-	
-	
-	this.run();
 }
 
+/* Switch to phaser */
+
+peggle.prototype.preload = function () {
+	this.game.load.image('background', 'assets/images/background-1.jpg');
+	this.game.load.image('ball-unhit', 'assets/images/ball-unhit-16px.png');
+	this.game.load.image('ball-hit', 'assets/images/ball-hit-16px.png');
+	this.game.load.image('shutter', 'assets/images/shutter-16px.png');
+	this.game.load.image('arrow', 'assets/images/arrow.png');
+}
+
+peggle.prototype.create = function () {
+	this.game.physics.setBoundsToWorld(true, true, true, false);
+	this.game.add.sprite(0, 0, 'background');
+	
+	PeggleGame.shutter = this.game.add.sprite(504, 100, 'shutter');
+	PeggleGame.shutter.anchor.setTo(0.5, 0.5);
+	PeggleGame.shutter.collideWorldBounds = true;
+	
+	//Set bounce (on bounce element contiune with 90% of current speed
+	PeggleGame.shutter.body.bounce.setTo(0.9, 0.9);
+	
+	//Set element "hitbox"
+	PeggleGame.shutter.body.setCircle(8, 8, 8);
+	
+	//the ball slows down by itself at this rate
+	PeggleGame.shutter.body.linearDamping = 1.0;
+	
+	//used to check if the ball has come to a hold
+	PeggleGame.shutter.body.minVelocity.setTo(1, 1);
+
+	
+	PeggleGame.appendBalls();
+	
+	PeggleGame.createArrow();
+	
+	this.game.input.onUp.add(function () {
+		PeggleGame.leftClick();
+	});
+}
+
+peggle.prototype.update = function () {
+ //mode 0 means the ball is moving
+  if (PeggleGame.isMoving == true) {
+
+    //if the ball is moving, remove all info texts if there are any
+
+    //collide the ball with the walls
+    this.game.physics.collide(PeggleGame.shutter, PeggleGame.elements.webbles, function () {
+		console.log('?');
+	}, null, this);
+
+    //Check if the ball is moving so slow that we can make it stop completly and switch to swing-mode
+    if (Math.abs(PeggleGame.shutter.body.velocity.x) < PeggleGame.shutter.body.minVelocity.x && Math.abs(PeggleGame.shutter.body.velocity.y) < PeggleGame.shutter.body.minVelocity.y) {
+      PeggleGame.isMoving = false;
+    }
+
+  } else if (PeggleGame.isMoving == false) {
+    //mode 1 is the swing mode, the ball is laying still
+
+    //rotate the arrow according to mouse/finger position
+    PeggleGame.elements.arrow.rotation = this.game.math.angleBetween(PeggleGame.shutter.body.x, PeggleGame.shutter.body.y, this.game.input.x, this.game.input.y) + Math.PI/2;
+
+    
+  }
+}
+
+
+
+
+peggle.prototype.appendBalls = function () {
+	this.elements.webbles = this.game.add.group();
+	var x, y;
+	//
+	for (var i = 0; i < this.elementsCount; i++) {
+		
+		
+		
+		var x = Math.floor(Math.random()* this.maxX);
+		var y = Math.floor(Math.random()* this.maxY);
+		
+		
+		
+		var currentBall = this.elements.webbles.create(x, y, 'ball-unhit');
+		currentBall.body.immovable = true;
+	}
+}
+
+peggle.prototype.createArrow = function () {
+		//create the direction arrow
+	this.elements.arrow = this.game.add.sprite(0, 0, 'arrow');
+	this.elements.arrow.anchor.setTo(0.5, 0.5);
+	this.elements.arrow.pivot.x = 0;
+	this.elements.arrow.pivot.y = +35;
+	//add it as a child to the ball, so that it circles the ball
+	this.shutter.addChild(this.elements.arrow);
+}
+
+peggle.prototype.removeArrow = function () {
+	this.shutter.removeChild(this.elements.arrow);
+	this.elements.arrow.destroy();
+}
+
+
+/* actions */
+peggle.prototype.leftClick = function () {
+	this.shutter.body.velocity = this.game.physics.accelerationFromRotation(this.elements.arrow.rotation - Math.PI/2, 300);
+	this.isMoving = true;
+}
+
+
+
+
+
+
+
+
+/*
+ * 
+ * @returns {undefined}
+
 peggle.prototype.run = function () {
-	this.render();
-	this.actions();
-	this.start();
+	
+	
+//	this.render();
+//	this.actions();
+//	this.start();
 }
 
 peggle.prototype.render = function () {
@@ -103,22 +226,7 @@ peggle.prototype.appendShutter = function () {
 	this.stage.addChild(shutter);
 }
 
-peggle.prototype.appendBalls = function () {
 
-	//
-	for (var i = 0; i < 10; i++) {
-		var current = this.getBall(i);
-		
-		// center the sprites anchor point
-		current.anchor.x = 0.5;
-		current.anchor.y = 0.5;
-		
-		current.position.x = Math.floor(Math.random()* this.maxX);
-		current.position.y = Math.floor(Math.random()* this.maxY);
-		
-		this.stage.addChild(current);
-	}
-}
 
 peggle.prototype.animate = function () {
 	var self = this;
@@ -131,9 +239,9 @@ peggle.prototype.animate = function () {
 	// render the stage   
 	self.renderer.render(self.stage);
 }
+ */
 
-
-/* Events */
+/* Events 
 peggle.prototype.fireBall = function () {
 	var moving = true;
 	var shutter = this.getShutter();
@@ -149,9 +257,9 @@ peggle.prototype.fireBall = function () {
 	}
 }
 
+*/
 
-
-/* Elements */
+/* Elements 
 peggle.prototype.getShutter = function () {
 	if (this.elements['shutter']) { 
 		return this.elements['shutter'];
@@ -183,7 +291,7 @@ peggle.prototype.getBall = function (item) {
 	
 	return this.elements['balls'][item]
 }
-
+*/
 
 
 	
